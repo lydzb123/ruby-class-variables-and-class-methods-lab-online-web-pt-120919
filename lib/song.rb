92099d30baa31b186093faf9177e5552c2fc2332
Lydia B <lydiabaik@gmail.com>
2020-01-10 07:49:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   def self.genre_count
     genre_count = Hash.new(0)
     @@genres.each do |genre|
-    genre_count[genre] += 1
+      genre_count[genre] += 1
     end
     return genre_count
   end
@@ -44,7 +44,6 @@ class Song
       artist_count[artist] += 1
     end
     return artist_count
-
   end
 
 
